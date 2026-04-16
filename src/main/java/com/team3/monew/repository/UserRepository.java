d8659d63c7eaf6c1efee55df9cf194ca3b4880be
JunSuHwang <wnstn9904@naver.com>
2026-04-16 16:43:54 +0900

    feat: 사용자 등록 Repository 구현
    feat: #63

---
+++ src/main/java/com/team3/monew/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.team3.monew.repository;
+
+import com.team3.monew.entity.User;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+  boolean existsByEmail(String email);
+}
