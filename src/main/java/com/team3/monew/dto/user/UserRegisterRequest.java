1dc648efb69faabe13558853574f8bf1842518ba
JunSuHwang <wnstn9904@naver.com>
2026-04-17 11:06:56 +0900

    feat: 비밀번호 검증 실패 메세지 개선

--- src/main/java/com/team3/monew/dto/user/UserRegisterRequest.java
+++ src/main/java/com/team3/monew/dto/user/UserRegisterRequest.java
@@ -19,6 +19,7 @@ public record UserRegisterRequest(
     @NotBlank
     @Size(min = 8, max = 20)
     @Pattern(
+        message = "비밀번호는 영문자와 숫자를 포함해야 하며, 사용할 수 있는 특수문자는 !@#$%^&*()_+ 입니다.",
         regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d!@#$%^&*()_+]+$"
     )
     String password
