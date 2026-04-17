ec1db1cffd6eca18033fb6bf806efeec3ecbb93a
김현민 <rlagusals911@naver.com>
2026-04-17 14:41:45 +0900

    댓글 등록 컨트롤러 작성

--- src/main/java/com/team3/monew/controller/CommentController.java
+++ src/main/java/com/team3/monew/controller/CommentController.java
@@ -1,10 +1,30 @@
 package com.team3.monew.controller;
 
+import com.team3.monew.controller.api.CommentApi;
+import com.team3.monew.dto.data.CommentDto;
+import com.team3.monew.dto.request.CommentRegisterRequest;
+import com.team3.monew.service.CommentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/comments")
-public class CommentController {
+public class CommentController implements CommentApi {
 
+  private final CommentService commentService;
+
+  @PostMapping
+  public ResponseEntity<CommentDto> registerComment(
+      @Valid @RequestBody CommentRegisterRequest commentRegisterRequest
+  ) {
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(commentService.registerComment(commentRegisterRequest));
+  }
 }
---
+++ src/main/java/com/team3/monew/controller/api/CommentApi.java
@@ -0,0 +1,25 @@
+package com.team3.monew.controller.api;
+
+import com.team3.monew.dto.data.CommentDto;
+import com.team3.monew.dto.request.CommentRegisterRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "댓글 관리", description = "댓글 관리 API")
+public interface CommentApi {
+
+  @Operation(summary = "댓글 등록", description = "새로운 댓글을 등록합니다.")
+  @ApiResponses({
+      @ApiResponse(responseCode = "201", description = "댓글 등록 성공"),
+      @ApiResponse(responseCode = "400", description = "잘못된 요청(입력값 검증 실패)"),
+      @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+  })
+  ResponseEntity<CommentDto> registerComment(
+      @Valid @RequestBody CommentRegisterRequest commentRegisterRequest
+  );
+}
