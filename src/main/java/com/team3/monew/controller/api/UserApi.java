5975f1fac8df24dd6576d264a2d45d2884c65087
JunSuHwang <wnstn9904@naver.com>
2026-04-19 20:23:36 +0900

    feat: 로그인 기능 api 구현

--- src/main/java/com/team3/monew/controller/api/UserApi.java
+++ src/main/java/com/team3/monew/controller/api/UserApi.java
@@ -1,6 +1,7 @@
 package com.team3.monew.controller.api;
 
 import com.team3.monew.dto.user.UserDto;
+import com.team3.monew.dto.user.UserLoginRequest;
 import com.team3.monew.dto.user.UserRegisterRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -21,5 +22,13 @@ public interface UserApi {
       @ApiResponse(responseCode = "500", description = "서버 내부 오류")
   })
   ResponseEntity<UserDto> registerUser(@Valid @RequestBody UserRegisterRequest userRegisterRequest);
+
+  @Operation(summary = "로그인", description = "사용자 로그인을 처리합니다.")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "로그인 성공"),
+      @ApiResponse(responseCode = "400", description = "잘못된 요청(입력값 검증 실패)"),
+      @ApiResponse(responseCode = "401", description = "로그인 실패(이메일 또는 비밀번호 불일치")
+  })
+  ResponseEntity<UserDto> loginUser(@Valid @RequestBody UserLoginRequest userLoginRequest);
 
 }
