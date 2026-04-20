eb61a4ca13ab5fba762691fd84e005d3712bcce2
JunSuHwang <wnstn9904@naver.com>
2026-04-20 09:38:54 +0900

    feat: 서비스 로깅 추가

--- src/main/java/com/team3/monew/service/UserService.java
+++ src/main/java/com/team3/monew/service/UserService.java
@@ -9,11 +9,13 @@ import com.team3.monew.exception.user.DuplicateEmailException;
 import com.team3.monew.mapper.UserMapper;
 import com.team3.monew.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -25,6 +27,7 @@ public class UserService {
 
   @Transactional
   public UserDto registerUser(UserRegisterRequest userRegisterRequest) {
+    log.debug("사용자 등록 요청: email={}", userRegisterRequest.email());
 
     validateDuplicateEmail(userRegisterRequest.email());
 
@@ -34,6 +37,7 @@ public class UserService {
 
     // 동시성 문제로 DB레벨 중복 예외 발생 시 예외 전환
     try {
+      log.debug("사용자 등록 성공: userId={}, email={}", user.getId(), user.getEmail());
       return userMapper.toDto(userRepository.save(user));
     } catch (DataIntegrityViolationException e) {
       throw new DuplicateEmailException();
@@ -41,12 +45,14 @@ public class UserService {
   }
 
   public UserDto loginUser(UserLoginRequest userLoginRequest) {
+    log.debug("사용자 로그인 요청: email={},", userLoginRequest.email());
     User user = userRepository.findByEmail(userLoginRequest.email())
         .orElseThrow(AuthException::new);
 
     if (!passwordEncoder.matches(userLoginRequest.password(), user.getPassword())) {
       throw new AuthException();
     }
+    log.debug("사용자 로그인 성공: userId={}, email={}", user.getId(), user.getEmail());
     return userMapper.toDto(user);
   }
 
