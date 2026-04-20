9b0f80b42b580afba6b5e619806528c03a02d33c
Minhyeong-Kim <144206885+Minbro-Kim@users.noreply.github.com>
2026-04-20 15:19:40 +0900

    feat: 댓글 좋아요 알림 등록 기능 (#116)
    * feat: 댓글 좋아요 Event 정의 및 리스너 구현
    
    - CommentLikedEvent 생성
    - AsyncConfig 클래스 추가
      - 비동기 설정
    - NotificationService 생성
    - NotificationEventListener 구현
     - 좋아요 이벤트를 수신하는 이벤트 리스너 메소드 정의
    
    - NotificationEventListenerTest 작성
    - NotificationEventListenerIntegrationTest 작성
    
    * refactor: 기존 개별 파라미터 방식에서 CommentLikedNotificationRequest Dto로 변경
    
    - NotificationEventListener에서 파라미터로 서비스를 호출하는 방식에서 DTO 방식으로 변경
    - NotificationService 메서드 시그니처 변경
    - 관련 테스트 코드 수정
    
    Closes: #104
    
    * refactor: Notification 엔티티 내 필요한 검증 추가 및 변경
    
    - 중복 필드 제거(notificationResourceType과 notificationType의 역할이 중복)
    - 중복 되는 create 메서드를 하나로 변경
    - 필드 변경에 맞추어 sql 파일 변경
    Closes: #105
    
    * refactor: NotificationEventListenerIntegrationTest에 integration 태그 추가
    
    * refactor: CommentLikedEvent 발행 시, 작성자 아이디 제외
    
    - 댓글 아이디로 작성자를 알 수 있음으로 작성자 아이디 제외
    
    * feat: 좋아요 알림 서비스 저장 로직 구현
    
    - NotificationRepository 인터페이스 작성
    - NotificationService에서 댓글 알림 저장 메서드 작성
    - 이벤트에서 주어진 데이터를 엔티티로 변경 및 저장 로직 구현
    - 필수값 누락 등 예외 상황에 대한 커스텀 예외 처리
    - 관련 단위테스트 및 통합테스트 작성
    Closes: #96, #106
    
    * refactor: Notification 생성자에 널 방어 추가
    
    Closes: #118
    
    * refactor: 중복 예외처리 수정 및 댓글 작성자 널 체크 삭제
    
    - 중복된 예외 코드를 삭제
    - UserNotFound 얘외를 발생시킬때 아이디를 포함하도록 수정
    - 댓글 Entity에서 작성자를 반드시 가지기 때문에 서비스에서 널체크 삭제
      - 관련 테스트코드 삭제
    
    * refactor: NotificationIntegrationTest에서 리포지토리 비우기 순서 조정
    
    * refactor: NotificationServiceTest의 실패케이스에서 검증 부분 추가
    
    - 레포지토리에 저장 되지 않는 것을 확인
    Closes: #119
    
    * style: 불필요한 임포트문 삭제
    
    * refactor: 비동기 스레드풀 설정 및 리스너 예외처리
    
    - AsyncConfig 수정
     - 좋아요를 위한 스레드풀 정의
     - 비동기 예외 오버라이드
    - EventListener 수정
     - 커스텀 스레드풀 사용으로 수정
     - 예외 처리 추가
    - 관련 테스트 코드 추가
    
    * refactor: CommentNotFoundException에서 댓글 아이디를 파라미터로 받도록 수정
    
    * test: NotificationEventListenerIntegrationTest에서 스레드 풀 확인 로직 추가
    
    * style: 불필요한 임포트문 제거
    
    * refactor: 스레드 풀 종료 설정 추가
    
    * refactor: 리스너 재시도 기준을 DB 오류로 최소화
    
    * style: 불필요한 import문 제거
    
    * refactor: 좋아요 알림 리스너의 허용 예외를 커스텀에서 실제 서비스 발생 예외로 축소
    
    * refactor: Notification의 actorUser 필드가 좋아요 알림 타입이 아닌 경우 null 강제
    
    * refactor: 스래드 풀 생성 메서드 명 동사형으로 변경
    
    * test: NotificationIntegrationTest 성공 케이스 필드 검증 조건 추가
    
    * refactor: AsyncConfig에서 비동기 예외 처리 중, 파라미터 로깅 로직 추가
    
    - 파라미터의 모든 정보가 로그에 남지 않도록 메서드 이름만 나오도록 수정

---
+++ src/test/java/com/team3/monew/listener/NotificationEventListenerTest.java
@@ -0,0 +1,96 @@
+package com.team3.monew.listener;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.willThrow;
+
+import com.team3.monew.dto.user.notification.CommentLikedNotificationRequest;
+import com.team3.monew.event.CommentLikedEvent;
+import com.team3.monew.exception.comment.CommentNotFoundException;
+import com.team3.monew.exception.user.UserNotFoundException;
+import com.team3.monew.service.NotificationService;
+import java.util.UUID;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@Tag("unit")
+@ExtendWith(MockitoExtension.class)
+class NotificationEventListenerTest {
+
+  @Mock
+  private NotificationService notificationService;
+
+  @InjectMocks
+  private NotificationEventListener notificationEventListener;
+
+  @Test
+  @DisplayName("리스너가 댓글 좋아요 이벤트를 받으면 서비스의 댓글 좋아요 알림 등록 메서드를 호출한다.")
+  void shouldCallRegisterLikeNotificationMethod_whenListenCommentLikedEvent() {
+    // given
+    UUID actorUserId = UUID.randomUUID();
+    UUID commentId = UUID.randomUUID();
+    CommentLikedNotificationRequest request = new CommentLikedNotificationRequest(actorUserId,
+        commentId);
+    CommentLikedEvent event = new CommentLikedEvent(actorUserId, commentId);
+
+    // when
+    notificationEventListener.handleCommentLikedEvent(event);
+
+    // then
+    then(notificationService).should()
+        .registerLikeNotification(eq(request));
+  }
+
+  @Test
+  @DisplayName("알림 등록 중 댓글 예외가 발생하면 리스너가 처리하고 예외를 외부로 전파하지 않는다.")
+  void shouldCatchAndHandleCommentNotFoundsException_whenServiceThrowsIt() {
+    // given
+    CommentLikedEvent event = new CommentLikedEvent(UUID.randomUUID(), UUID.randomUUID());
+
+    // 서비스에서 댓글을 찾지 못함
+    willThrow(new CommentNotFoundException(UUID.randomUUID()))
+        .given(notificationService)
+        .registerLikeNotification(any(CommentLikedNotificationRequest.class));
+
+    // when & then
+    assertDoesNotThrow(() -> notificationEventListener.handleCommentLikedEvent(event));
+    then(notificationService).should().registerLikeNotification(any());
+  }
+
+  @Test
+  @DisplayName("알림 등록 중 사용자 예외가 발생하면 리스너가 처리하고 예외를 외부로 전파하지 않는다.")
+  void shouldCatchAndHandleUserNotFoundsException_whenServiceThrowsIt() {
+    // given
+    CommentLikedEvent event = new CommentLikedEvent(UUID.randomUUID(), UUID.randomUUID());
+
+    // 서비스에서 댓글을 찾지 못함
+    willThrow(new UserNotFoundException(UUID.randomUUID()))
+        .given(notificationService)
+        .registerLikeNotification(any(CommentLikedNotificationRequest.class));
+
+    // when & then
+    assertDoesNotThrow(() -> notificationEventListener.handleCommentLikedEvent(event));
+    then(notificationService).should().registerLikeNotification(any());
+  }
+
+  @Test
+  @DisplayName("알림 등록 중 커스텀 예외 외의 예외가 발생하면 리스너는 외부로 전파한다.")
+  void shouldThrowsException_whenServiceThrowsIt() {
+    // given
+    CommentLikedEvent event = new CommentLikedEvent(UUID.randomUUID(), UUID.randomUUID());
+    willThrow(new RuntimeException("예상치 못한 예외"))
+        .given(notificationService).registerLikeNotification(any());
+
+    // when & then
+    assertThrows(RuntimeException.class,
+        () -> notificationEventListener.handleCommentLikedEvent(event));
+  }
+}
---
+++ src/test/java/com/team3/monew/service/NotificationServiceTest.java
@@ -0,0 +1,132 @@
+package com.team3.monew.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+import com.team3.monew.dto.user.notification.CommentLikedNotificationRequest;
+import com.team3.monew.entity.Comment;
+import com.team3.monew.entity.NewsArticle;
+import com.team3.monew.entity.Notification;
+import com.team3.monew.entity.User;
+import com.team3.monew.entity.enums.NotificationResourceType;
+import com.team3.monew.exception.comment.CommentNotFoundException;
+import com.team3.monew.exception.user.UserNotFoundException;
+import com.team3.monew.repository.CommentRepository;
+import com.team3.monew.repository.NotificationRepository;
+import com.team3.monew.repository.UserRepository;
+import java.util.Optional;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@Tag("unit")
+@ExtendWith(MockitoExtension.class)
+class NotificationServiceTest {
+
+  @Mock
+  private NotificationRepository notificationRepository;
+
+  @Mock
+  private UserRepository userRepository;
+
+  @Mock
+  private CommentRepository commentRepository;
+
+
+  @InjectMocks
+  private NotificationService notificationService;
+
+  private UUID userId;
+  private UUID actorUserId;
+  private UUID resourceId;
+  private User user;
+  private User actorUser;
+
+  @BeforeEach
+  void setUp() {
+    userId = UUID.randomUUID();
+    actorUserId = UUID.randomUUID();
+    resourceId = UUID.randomUUID();
+
+    user = User.create("test@test.com", "test", "password");
+    actorUser = User.create("actor@test.com", "actor", "password");
+    ReflectionTestUtils.setField(user, "id", userId);
+    ReflectionTestUtils.setField(actorUser, "id", actorUserId);
+  }
+
+  @Test
+  @DisplayName("댓글, 작성자, 사용자 아이디로 좋아요 알림 등록에 성공합니다.")
+  void shouldRegisterLikeNotification() {
+    // given
+    UUID commentId = resourceId;
+    NewsArticle article = Mockito.mock(NewsArticle.class);
+    Comment comment = Comment.create(article, user, "test");
+    UUID writerId = userId;
+    CommentLikedNotificationRequest request = new CommentLikedNotificationRequest(actorUserId,
+        commentId);
+
+    given(commentRepository.findById(commentId)).willReturn(Optional.of(comment));
+    given(userRepository.findById(actorUserId)).willReturn(Optional.of(actorUser));
+
+    // when
+    notificationService.registerLikeNotification(request);
+
+    // then
+    ArgumentCaptor<Notification> notificationCaptor = ArgumentCaptor.forClass(Notification.class);
+    then(notificationRepository).should().save(notificationCaptor.capture());
+    Notification notification = notificationCaptor.getValue();
+
+    assertNotNull(notification);
+    assertEquals(commentId, notification.getResourceId());
+    assertEquals(NotificationResourceType.COMMENT, notification.getResourceType());
+    assertEquals(writerId, notification.getUser().getId());
+    assertEquals(actorUserId, notification.getActorUser().getId());
+    assertFalse(notification.isConfirmed());
+    assertEquals(actorUser.getNickname() + "님이 나의 댓글을 좋아합니다.", notification.getContent());
+  }
+
+  @Test
+  @DisplayName("좋아요를 누른 사용자를 찾을 수 없을 때 좋아요 알림 등록에 실패합니다.")
+  void shouldThrowException_whenActorUserNotFound() {
+    // given
+    UUID commentId = resourceId;
+    NewsArticle article = Mockito.mock(NewsArticle.class);
+    Comment comment = Comment.create(article, user, "test");
+    CommentLikedNotificationRequest request = new CommentLikedNotificationRequest(actorUserId,
+        commentId);
+
+    given(commentRepository.findById(commentId)).willReturn(Optional.of(comment));
+    given(userRepository.findById(actorUserId)).willReturn(Optional.empty());
+
+    // when & then
+    assertThrows(UserNotFoundException.class,
+        () -> notificationService.registerLikeNotification(request));
+    then(notificationRepository).shouldHaveNoInteractions();
+  }
+
+  @Test
+  @DisplayName("좋아요를 받은 댓글을 찾을 수 없을 때 좋아요 알림 등록에 실패합니다.")
+  void shouldThrowException_whenCommentNotFound() {
+    // given
+    UUID commentId = resourceId;
+    CommentLikedNotificationRequest request = new CommentLikedNotificationRequest(actorUserId,
+        commentId);
+
+    given(commentRepository.findById(commentId)).willReturn(Optional.empty());
+
+    // when & then
+    assertThrows(CommentNotFoundException.class,
+        () -> notificationService.registerLikeNotification(request));
+    then(notificationRepository).shouldHaveNoInteractions();
+  }
+}
