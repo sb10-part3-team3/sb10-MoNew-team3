1e6d87e3713849c90c8a88e8d73c5b196162068e
JunSuHwang <wnstn9904@naver.com>
2026-04-20 18:21:17 +0900

    feat: 사용자 수정 컨트롤러 및 요청 Dto
    feat: #123

--- src/main/java/com/team3/monew/controller/UserController.java
+++ src/main/java/com/team3/monew/controller/UserController.java
@@ -4,11 +4,15 @@ import com.team3.monew.controller.api.UserApi;
 import com.team3.monew.dto.user.UserDto;
 import com.team3.monew.dto.user.UserLoginRequest;
 import com.team3.monew.dto.user.UserRegisterRequest;
+import com.team3.monew.dto.user.UserUpdateRequest;
 import com.team3.monew.service.UserService;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +38,13 @@ public class UserController implements UserApi {
   ) {
     UserDto userDto = userService.loginUser(userLoginRequest);
     return ResponseEntity.ok(userDto);
+  }
+
+  @PatchMapping("/{userId}")
+  public ResponseEntity<UserDto> updateUser(
+      @PathVariable UUID userId,
+      @Valid @RequestBody UserUpdateRequest userUpdateRequest
+  ) {
+    return ResponseEntity.ok(userService.updateUser(userId, userUpdateRequest));
   }
 }
