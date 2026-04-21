07e3ac67d3f53fc6c642c579acb440a3e0ace39b
Minbro-Kim <sojangjangi@gmail.com>
2026-04-21 15:09:05 +0900

    refactor: InterestNotificationRequest의 구독자 아이디 리스트 방어직 복사 추가

--- src/main/java/com/team3/monew/dto/notification/InterestNotificationRequest.java
+++ src/main/java/com/team3/monew/dto/notification/InterestNotificationRequest.java
@@ -1,5 +1,6 @@
 package com.team3.monew.dto.notification;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -10,4 +11,7 @@ public record InterestNotificationRequest(
     List<UUID> subscriberIds
 ) {
 
+  public InterestNotificationRequest {
+    subscriberIds = subscriberIds == null ? new ArrayList<UUID>() : List.copyOf(subscriberIds);
+  }
 }
