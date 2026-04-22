0abe86c88fad19afba080992a0c8c15a9ba02532
Minbro-Kim <sojangjangi@gmail.com>
2026-04-22 15:29:12 +0900

    feat: 알림 엔터티 매퍼 작성
    closes: #165

---
+++ src/main/java/com/team3/monew/mapper/NotificationMapper.java
@@ -0,0 +1,16 @@
+package com.team3.monew.mapper;
+
+import com.team3.monew.dto.notification.CursorPageResponseNotificationDto.NotificationDto;
+import com.team3.monew.entity.Notification;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface NotificationMapper {
+
+  @Mappings({
+      @Mapping(source = "notification.user.id", target = "userId")
+  })
+  NotificationDto toDto(Notification notification);
+}
