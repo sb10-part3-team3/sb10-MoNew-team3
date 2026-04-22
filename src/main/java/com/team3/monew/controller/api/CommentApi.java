f64de6abb80a0006079ee33155331ba268a60e68
김현민 <rlagusals911@naver.com>
2026-04-22 10:37:47 +0900

    feat: 댓글 등록 컨트롤러 작성

--- src/main/java/com/team3/monew/controller/api/CommentApi.java
+++ src/main/java/com/team3/monew/controller/api/CommentApi.java
@@ -3,16 +3,19 @@ package com.team3.monew.controller.api;
 import com.team3.monew.dto.comment.CommentDto;
 import com.team3.monew.dto.comment.CommentRegisterRequest;
 import com.team3.monew.dto.comment.CommentUpdateRequest;
+import com.team3.monew.dto.comment.CursorPageResponseCommentDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.time.Instant;
 import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "댓글 관리", description = "댓글 관리 API")
 public interface CommentApi {
@@ -65,5 +68,21 @@ public interface CommentApi {
   })
   ResponseEntity<Void> hardDeleteComment(
       @PathVariable UUID commentId
+  );
+
+  @Operation(summary = "댓글 목록 조회", description = "기사별 댓글 목록을 정렬 조건에 따라 커서 기반으로 조회합니다.")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "조회 성공"),
+      @ApiResponse(responseCode = "400", description = "잘못된 요청(헤더 누락, 정렬 기준 오류, 페이지네이션 파라미터 오류 등)"),
+      @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+  })
+  ResponseEntity<CursorPageResponseCommentDto> findAllComments(
+      @RequestParam(required = false) UUID articleId,
+      @RequestParam String orderBy,
+      @RequestParam String direction,
+      @RequestParam(required = false) String cursor,
+      @RequestParam(required = false) Instant after,
+      @RequestParam int limit,
+      @RequestHeader(REQUEST_USER_ID_HEADER) UUID requestUserId
   );
 }
