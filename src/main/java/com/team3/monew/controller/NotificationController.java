5f69d8bd6362a9e82f6288f5db824a9704a26fab
Minbro-Kim <sojangjangi@gmail.com>
2026-04-23 17:57:52 +0900

    refactor: NotificationController 목록 조회 시, 요청 요소 수 최소 최대 값 설정

--- src/main/java/com/team3/monew/controller/NotificationController.java
+++ src/main/java/com/team3/monew/controller/NotificationController.java
@@ -4,10 +4,13 @@ import com.team3.monew.controller.api.NotificationApi;
 import com.team3.monew.dto.notification.NotificationDto;
 import com.team3.monew.dto.pagination.CursorPageResponseDto;
 import com.team3.monew.service.NotificationService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import java.time.Instant;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/api/notifications")
 public class NotificationController implements NotificationApi {
 
@@ -26,7 +30,7 @@ public class NotificationController implements NotificationApi {
       @RequestHeader("Monew-Request-User-ID") UUID requestUserId,
       @RequestParam(required = false) UUID cursor,
       @RequestParam(required = false) Instant after,
-      @RequestParam(defaultValue = "50") Integer limit
+      @RequestParam(defaultValue = "50") @Min(1) @Max(100) Integer limit
   ) {
     CursorPageResponseDto<NotificationDto> dto = notificationService.findAllNotConfirmed(
         requestUserId,
