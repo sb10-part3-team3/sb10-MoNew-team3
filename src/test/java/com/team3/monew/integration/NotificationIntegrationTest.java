793eb41368e2f47511cb3c8da381b2099eb5e1de
Minbro-Kim <sojangjangi@gmail.com>
2026-04-23 16:50:00 +0900

    test: 알림 목록 조회 통합 테스트 작성
    closes: #131

--- src/test/java/com/team3/monew/integration/NotificationIntegrationTest.java
+++ src/test/java/com/team3/monew/integration/NotificationIntegrationTest.java
@@ -3,6 +3,9 @@ package com.team3.monew.integration;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.team3.monew.dto.notification.CommentLikedNotificationRequest;
 import com.team3.monew.dto.notification.InterestNotificationRequest;
@@ -24,6 +27,8 @@ import com.team3.monew.repository.NotificationRepository;
 import com.team3.monew.repository.UserRepository;
 import com.team3.monew.service.NotificationService;
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
@@ -34,7 +39,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 @Tag("integration")
 @SpringBootTest
@@ -62,6 +71,9 @@ public class NotificationIntegrationTest {
 
   @Autowired
   private InterestRepository interestRepository;
+
+  @Autowired
+  private MockMvc mockMvc;
 
   @Nested
   @DisplayName("좋아요 알림 등록 테스트")
@@ -229,5 +241,125 @@ public class NotificationIntegrationTest {
           })
       );
     }
+  }
+
+  @Nested
+  @DisplayName("알림 API 테스트")
+  @Transactional
+  class NotificationControllerTest {
+
+    private User subscriber;
+    private User actor;
+    private Comment comment;
+    private NewsArticle article;
+
+    private Interest interest1;
+    private Interest interest2;
+
+    private Notification notification1;
+    private Notification notification2;
+    private Notification notification3;
+    private Notification notification4;
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+            "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'")
+        .withZone(ZoneId.of("UTC"));
+
+    @BeforeEach
+    void setUp() {
+      notificationRepository.deleteAll();
+      commentRepository.deleteAll();
+      newsArticleRepository.deleteAll();
+      newsSourceRepository.deleteAll();
+      interestRepository.deleteAll();
+      userRepository.deleteAll();
+
+      subscriber = userRepository.save(User.create("subscriber0@test.com", "subscriber", "pwd"));
+      actor = userRepository.save(User.create("actor@test.com", "actor", "pwd"));
+      NewsSource newsSource = NewsSource.create("test", NewsSourceType.NAVER, "url");
+      newsSourceRepository.save(newsSource);
+      article = newsArticleRepository.save(
+          NewsArticle.create(newsSource, "link", "title", Instant.now(), "summary"));
+      comment = commentRepository.save(Comment.create(article, subscriber, "댓글 내용"));
+
+      interest1 = Interest.create("interest1");
+      interest2 = Interest.create("interest2");
+      interestRepository.saveAll(List.of(interest1, interest2));
+
+      notification1 = Notification.create(subscriber, "content1", NotificationResourceType.COMMENT,
+          comment.getId(), actor);
+      notification2 = Notification.create(subscriber, "content2", NotificationResourceType.INTEREST,
+          interest1.getId(), null);
+      notification3 = Notification.create(subscriber, "content3", NotificationResourceType.INTEREST,
+          interest2.getId(), null);
+      notification4 = Notification.create(subscriber, "content4", NotificationResourceType.COMMENT,
+          comment.getId(), actor);//이미 확인된
+      ReflectionTestUtils.setField(notification4, "isConfirmed", true);
+      notificationRepository.saveAll(
+          List.of(notification1, notification2, notification3, notification4));
+    }
+
+    @Test
+    @DisplayName("모든 파라미터 없이 알림 목록을 조회한다.")
+    void shouldFindAllNotificationsNotConfirmed_whenAnythingIsGiven() throws Exception {
+      mockMvc.perform(get("/api/notifications")
+              .header("Monew-Request-User-ID", subscriber.getId())
+              .accept(MediaType.APPLICATION_JSON))
+          .andExpectAll(
+              status().isOk(),
+              jsonPath("$.content.size()").value(3),
+              jsonPath("$.content[0].resourceId").value(interest2.getId().toString()),//저장 역순
+              jsonPath("$.content[1].resourceId").value(interest1.getId().toString()),
+              jsonPath("$.content[2].resourceId").value(comment.getId().toString()),
+              jsonPath("$.size").value(50),//기본 요청 리미트
+              jsonPath("$.totalElements").value(3),//확인안된 알림 3개
+              jsonPath("$.hasNext").value(false)
+          );
+    }
+
+    @Test
+    @DisplayName("커서 조건으로 알림 목록을 조회한다.")
+    void shouldFindAllNotificationsNotConfirmed_whenCursorConditionsAreGiven() throws Exception {
+      //가장 나중에 저장된 notification3을 보고 이후 목록을 요청(확인 안함 기준)
+      mockMvc.perform(get("/api/notifications")
+              .header("Monew-Request-User-ID", subscriber.getId())
+              .param("cursor", notification3.getId().toString())
+              .param("after", notification3.getCreatedAt().toString())
+              .param("limit", "5")
+              .accept(MediaType.APPLICATION_JSON))
+          .andExpectAll(
+              status().isOk(),
+              jsonPath("$.content.size()").value(2),
+              jsonPath("$.content[0].resourceId").value(interest1.getId().toString()),
+              jsonPath("$.content[1].resourceId").value(comment.getId().toString()),
+              jsonPath("$.size").value(5),//요청 리미트
+              jsonPath("$.totalElements").value(3),//전체 미확인 알림 개수는 3개
+              jsonPath("$.hasNext").value(false)
+          );
+    }
+
+    @Test
+    @DisplayName("커서 조건으로 다음 페이지가 있는 알림 목록을 조회한다.")
+    void shouldFindAllNotificationsNotConfirmed_whenCursorConditionsAreGivenWithHasNext()
+        throws Exception {
+      //가장 나중에 저장된 notification3을 보고 이후 목록을 요청(확인 안함 기준)
+      mockMvc.perform(get("/api/notifications")
+              .header("Monew-Request-User-ID", subscriber.getId())
+              .param("cursor", notification3.getId().toString())
+              .param("after", notification3.getCreatedAt().toString())
+              .param("limit", "1")
+              .accept(MediaType.APPLICATION_JSON))
+          .andExpectAll(
+              status().isOk(),
+              jsonPath("$.content.size()").value(1),
+              jsonPath("$.content[0].resourceId").value(interest1.getId().toString()),
+              jsonPath("$.size").value(1),//요청 리미트
+              jsonPath("$.totalElements").value(3),//전체 미확인 알림 개수는 3개
+              jsonPath("$.hasNext").value(true),
+              jsonPath("$.nextCursor").value(notification2.getId().toString()),//마지막 확인한 요소의 값
+              jsonPath("$.nextAfter").value(formatter.format(notification2.getCreatedAt()))
+          );
+    }
+
   }
 }
