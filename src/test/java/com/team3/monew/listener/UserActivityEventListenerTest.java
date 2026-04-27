adecc18e26ca80540a52d59e849cfe437306f9c3
JunSuHwang <wnstn9904@naver.com>
2026-04-27 11:01:33 +0900

    test: 리스너 테스트에 태그 추가

--- src/test/java/com/team3/monew/listener/UserActivityEventListenerTest.java
+++ src/test/java/com/team3/monew/listener/UserActivityEventListenerTest.java
@@ -20,12 +20,14 @@ import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+@Tag("unit")
 @ExtendWith(MockitoExtension.class)
 class UserActivityEventListenerTest {
 
